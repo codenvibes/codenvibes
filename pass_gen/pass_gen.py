326c988549c966d52632020fd4490a99cc4da63c
codenvibes <terrymk1999@gmail.com>
2024-02-03 13:09:24 +0300

    Docs: Add docstrings

--- pass_gen/pass_gen.py
+++ pass_gen/pass_gen.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 # AUTH: codenvibes
 """
+This script generates a random password of the specified length.
 """
 import random
 import string
 
 
 def generate_password(length=None):
+    """
+    Generates a random password of the specified length.
+
+    Args:
+    - length (int): The length of the password to generate. If not provided, the user will be prompted to enter a length.
+
+    Returns:
+    - str: The generated password.
+    """
     if length is None:
-        length = int(input("Enter password length: "))
+        length = int(input("Enter `password length: "))
         characters = string.ascii_letters + string.digits + string.punctuation
         password = "".join(random.choice(characters) for _ in range(length))
         return(password)
